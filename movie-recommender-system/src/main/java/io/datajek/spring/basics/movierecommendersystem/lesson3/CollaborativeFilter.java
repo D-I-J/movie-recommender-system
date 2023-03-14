9868778bb92a180599b41e8d4930622c3678b71b
Daniyal <f2018065166@umt.edu.pk>
2023-03-14 16:48:57 +0500

    Used spring boot to make beans

---
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson3/CollaborativeFilter.java
@@ -0,0 +1,11 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson3;
+
+import io.datajek.spring.basics.movierecommendersystem.lesson2.Filter;
+
+
+public class CollaborativeFilter implements Filter {
+    public String[] getRecommendations(String movie)
+    {
+        return new String[] {"Finding Nemo", "Ice Age", "Toy Story"};
+    }
+}
