44436638535aeea3937482c8c86d01dee005d954
Daniyal <f2018065166@umt.edu.pk>
2023-03-15 10:14:31 +0500

    Used @Primary annotation on CollaborativeFilter

---
+++ movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson4/RecommenderImplementation.java
@@ -0,0 +1,28 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson4;
+
+import io.datajek.spring.basics.movierecommendersystem.lesson2.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecommenderImplementation {
+
+    @Autowired
+    private Filter filter;
+
+    RecommenderImplementation(Filter filter)
+    {
+        super(); // unnecessary to write but causes no issue when written.
+        this.filter = filter;
+    }
+    public String[] recommendMovies(String movie)
+    {
+        System.out.println("Name of the filter in use: " + filter + "\n");
+        String[] results = filter.getRecommendations("Finding Dory");
+        return results;
+    }
+
+
+
+
+}
